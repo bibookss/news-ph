c1f9b083b7f627a2dc901d2092ead8acbed6ef34
Albert Perez <albperez@gbox.adnu.edu.ph>
2025-06-18 10:50:22 +0800

    chore: use env to store constants

---
+++ news_ph/assets/constants.py
@@ -0,0 +1,3 @@
+import os
+
+DUCKDB_PATH = os.getenv("DUCKDB_PATH")
--- news_ph/resources/__init__.py
+++ news_ph/resources/__init__.py
@@ -1,6 +1,8 @@
 from dagster_duckdb_pandas import DuckDBPandasIOManager
 
 from .abs_cbn import ABSCBNResource
+from ..assets import constants
+
 
 abs_cbn_resource = ABSCBNResource()
-duck_db_pandas_io = DuckDBPandasIOManager(database="news.duckdb")  # Set using env
+duck_db_pandas_io = DuckDBPandasIOManager(database=constants.DUCKDB_PATH)  #type: ignore
